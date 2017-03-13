d7d2214c45580b3ecc3cd47734ad050cc177f0c2
andrewmdinh <amd3ce@virginia.edu>
2017-03-13 16:42:25 -0400

    Pronto Website, Milestone 2
    Source code and necessary files, updated on Milestone 2

---
+++ signup.php
@@ -0,0 +1,33 @@
+<?php
+
+$connect=new mysqli('localhost','root','','pronto');
+
+
+if($connect->connect_error){
+	die("Connection failed: " . $connect->connect_error);
+}
+else{
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$address = $_POST["address"];
+	$city =  $_POST["city"];
+	$state = $_POST["state"];
+	$zip_code = $_POST["zip_code"];
+
+
+	$sql = "SELECT * FROM siteusers WHERE Name='$name'";
+	$result = $connect->query($sql) or trigger_error($connect->error."[$sql]");
+	
+	if ($result->num_rows>0)
+		{
+		echo 'You are already signed up!';
+	}
+	else{
+		$connect->query("INSERT INTO siteusers(Name, Email, Address, City, State, Zip) VALUES('$name', '$email', '$address', '$city', '$state','$zip_code')");
+
+		echo 'Thanks for signing up!';
+	}
+	
+}
+
+?>
