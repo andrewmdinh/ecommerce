7e10d0ba923248f1a8a785056a8227ddd02605c9
andrewmdinh <amd3ce@virginia.edu>
2017-03-22 23:36:52 -0400

    updated signup.php including email

--- Pronto - Updated (Danielle)/signup.php
+++ Pronto - Updated (Danielle)/signup.php
@@ -1,6 +1,36 @@
 <?php
+function sendMail($mail, $name, $email){
+	$mail->isSMTP();                                      // Set mailer to use SMTP
+	$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+	$mail->SMTPAuth = true;                               // Enable SMTP authentication
+	$mail->FromName = "Pronto";
+	$mail->Username = 'prontoenterprises7@gmail.com';                 // SMTP username
+	$mail->Password = 'pronto135';                           // SMTP password
+	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 587;                                    // TCP port to connect to
 
+	$mail->setFrom('pronto@gmail.com', 'Pronto');
+	$mail->addAddress('amd3ce@virginia.edu');     // Add a recipient
+
+	$mail->Subject = 'Thanks for signing up with Pronto!';
+	$mail->Body    = 'Get ready for convenience at your fingertips!';
+
+	if(!$mail->send()) {
+	    echo 'Message could not be sent.';
+	    echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+	    echo 'Message has been sent';
+	}
+}
+
+$mailpath = 'C:\xampp\htdocs';
+$path = get_include_path();
 $connect=new mysqli('localhost','root','','pronto');
+set_include_path($path . PATH_SEPARATOR . $mailpath);
+
+require 'PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
 
 
 if($connect->connect_error){
@@ -20,14 +50,16 @@ else{
 	
 	if ($result->num_rows>0)
 		{
-		#echo 'You are already signed up!';
-		<a href="registered.html">You have already registered</a>
+		echo 'You are already signed up!';
+		sendMail($mail, $name, $email);
+		#<li><a href="registered.html"></a></li>
 	}
 	else{
 		$connect->query("INSERT INTO siteusers(Name, Email, Address, City, State, Zip) VALUES('$name', '$email', '$address', '$city', '$state','$zip_code')");
 
-		#echo 'Thanks for signing up!';
-		<a href="newRegister.html">Thanks</a>
+		echo 'Thanks for signing up!';
+		sendMail($mail, $name, $email);
+		#<a href="newRegister.html">Thanks</a>
 	}
 	
 }
